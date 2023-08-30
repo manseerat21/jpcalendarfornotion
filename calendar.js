a50f55624053a73c88adb0a4efb3848fd912e8bc
Manseerat S. Gill <133496280+manseerat21@users.noreply.github.com>
2023-08-31 02:56:35 +0530

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,6 +1,7 @@
 const daysElement = document.getElementById("calendarDays");
 const fullDateElement = document.getElementById("fullDate");
 const clockElement = document.getElementById("clock");
+const japaneseTimeElement = document.getElementById("japaneseTime");
 
 const japaneseMonths = ["睦月", "如月", "弥生", "卯月", "皐月", "水無月", "文月", "葉月", "長月", "神無月", "霜月", "師走"];
 const japaneseDays = ["日", "月", "火", "水", "木", "金", "土"];
@@ -38,7 +39,12 @@ function generateCalendar(year, month, day) {
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
-  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+  const localTime = `${hours}:${minutes}:${seconds}`;
+  
+  const japaneseTime = new Date().toLocaleTimeString("ja-JP", { timeZone: "Asia/Tokyo" });
+  
+  clockElement.textContent = localTime;
+  japaneseTimeElement.textContent = japaneseTime;
 }
 
 const currentDate = new Date();
@@ -49,5 +55,10 @@ setInterval(() => {
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
-  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+  const localTime = `${hours}:${minutes}:${seconds}`;
+  
+  const japaneseTime = new Date().toLocaleTimeString("ja-JP", { timeZone: "Asia/Tokyo" });
+  
+  clockElement.textContent = localTime;
+  japaneseTimeElement.textContent = japaneseTime;
 }, 1000);
